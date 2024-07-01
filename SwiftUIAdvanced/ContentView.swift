0b98969717a494bc75f10cbc1b40b6c3b8d3fa68
TU NGUYEN <trongtunguyen08@icloud.com>
2024-07-01 15:13:44 +0700

    Initial Commit

---
+++ SwiftUIAdvanced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAdvanced
+//
+//  Created by Tu Nguyen on 1/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Tu Nguyen on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
